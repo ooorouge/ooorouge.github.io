d1b93283069a10a00b0e24c732e284e9c59b47ea
JingZhang <zjing@wustl.edu>
2020-12-08 13:54:44 -0600

    add instruction modal

--- js/main.js
+++ js/main.js
@@ -113,6 +113,38 @@ function updateTags() {
 $(document).ready(function() {
     $(this).scrollTop(0);
     indexInit();
+
+
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close");
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    for(var i = 0; i < span.length; i++) {
+        span[i].onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+
+
     var $window = $(window);
     var div2 = $('#map');
     var div1 = $('#maps');
